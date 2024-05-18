c15302579388b3dbacd422a85755a7bf9160fe2d
Jonathan Soto <37603928+JonathanDSoto@users.noreply.github.com>
2024-05-18 15:34:14 -0700

    optimización de post

--- src/models/AuthModel.java
+++ src/models/AuthModel.java
@@ -18,7 +18,27 @@ public class AuthModel {
 	public void login(String username, String password) {
 		
 		
-		  
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			Connection con=DriverManager.getConnection("jdbc:mysql://sql.freedb.tech:3306/freedb_super_data_17","freedb_master_of_data_test","mr2@d4rgDFKC7Ps");  
+			
+			Statement stmt=con.createStatement();  
+			
+			ResultSet rs=stmt.executeQuery("SELECT * FROM `users`");  
+			
+			while(rs.next())  
+				System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+"  "+rs.getDate(4));  
+			
+			con.close();
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
 		/*if(username.equals("admin") && password.equals("1234")) {
